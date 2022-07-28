ed268c5e502297ad1527db2473f6bb3ec68a3196
aaravironspiderman <96777258+aaravironspiderman@users.noreply.github.com>
2022-07-28 17:55:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload(){
+
+
+}
+
+function setup(){
+
+    canvas = createCanvas(350,350);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+
+
+}
+
+
+function draw(){
+
+
+
+}
